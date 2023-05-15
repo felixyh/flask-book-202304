7f4661b08493a1c9e2180a5280faed114f01fb9a
felixyh <felix_yanghui@sina.cn>
2023-05-15 18:17:30 +0800

    c5

--- Part-1/c4-web form/form4.py
+++ Part-1/c4-web form/form4.py
@@ -1,3 +1,11 @@
+# import FlaskForm, as Form is not supported in new flask_wtf
+# 具体是Form 还是 FlaskForm根据自己的项目情况版本来决定 
+# 这里注意下：from flask_wtf import FlaskForm,validators里是DataRequired，
+# 如果是from flask_wtf import Form 就对应是 Required了
+
+# from flask_wtf import Form
+
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
